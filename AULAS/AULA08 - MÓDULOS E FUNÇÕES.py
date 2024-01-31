dcb75dc092c16ffcb436233aae249f84e5de07e4
recepcao <moneycrashing76@gmail.com>
2024-01-31 17:00:38 -0300

    MÓDULOS/BIBLIOTECAS EM PYTHON - CURSO EM VÍDEO | GUSTAVO GUANABARA

---
+++ AULAS/AULA08 - MÓDULOS E FUNÇÕES.py
@@ -0,0 +1,27 @@
+#Módulos e Funções
+
+##Essas são duas maneiras de utilizar o comando import, sendo o primeiro
+##comando mais generalista
+
+#import -> Usado para importar a biblioteca que será utilizada
+
+#from <nome_da_biblioteca> import <nome_do_objeto>
+
+#Exemplos de Bibliotecas
+
+#math:
+#->ceil
+#->floor
+#->trunc
+#->pow
+#->sqrt
+#->factorial
+
+#módulo 'RANDOM'
+import random
+num = random.randint(1,10)
+print(num)
+
+
+#É POSSÍVEL TAMBÉM IMPORTAR BIBLIOTECAS NO PRÓPRIO SITE DO PYTHON
+##INSTALAR TAMBÉM MÓDULOS EXTRAS PARA UTILIZAR EM PYTHON
---
+++ AULAS/CHALLENGES/CHALLENGE16.py
@@ -0,0 +1,6 @@
+import math
+print('{}'.format('='*8), 'CHALLENGE16', '{}'.format('='*8))
+numeroReal = float(input('Digite um número: '))
+conversao = math.floor(numeroReal)
+print('O número {:.3f} tem a parte inteira {}'.format(numeroReal,conversao))
+print('{}'.format('='*8),' FIM ','{}'.format('='*8))
