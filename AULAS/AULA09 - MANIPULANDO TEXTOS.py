f10a42f906e293942e4c20b5f29a8c4233b856c4
recepcao <moneycrashing76@gmail.com>
2024-02-02 11:34:22 -0300

    Inserção de arquivo explicativo sobre a matéria

---
+++ AULAS/AULA09 - MANIPULANDO TEXTOS.py
@@ -0,0 +1,36 @@
+texto= 'Curso em video python'
+frase = '  Aprenda Python  '
+#Fatiamento
+print(texto[9]) #Imprime apenas o elemento do índice indicado
+print(texto[9:13]) ##Imprime os elementos do índice até o índice indicado menos um, ou seja, 13 - 1
+print(texto[9:21:2])##Nessa impressão temos o índice inicial,
+                          ##onde irá ser interrompida a busca e a quantidade de
+                          ##elementos que será pulada entre cada elemento que terá na saída
+print(texto[:5]) ##Quando omito o início, a contagem começa de 0
+print(texto[15:]) ##Indico o início mas omito o final e ele fatia até o final da string
+print(texto[9::3])##Indico o início e quantos elementos irá pular
+
+##Análise
+print(len(texto)) ##Imprime o tamanho da string
+print(texto.count('o')) ##O programa faz contagem de quantas vezes aparece a letra o minúscula
+print(texto.count('o',0,13)) ##Contagem juntamente com o fatiamento
+print(texto.find('deo')) ##Indica onde o termo começou
+print(texto.find('Android')) ##Retorna um valor -1, o que indica que a expressão não existe
+print('Curso' in texto) ##Retorna valor booleano
+
+##Transformação
+print(texto.replace('python','Android')) ##Substitui o termo 'python' pelo termo 'Android'
+print(texto.upper()) ##Torna o texto todo em letras maiúsculas e mantêm as que já estavam
+print(texto.lower()) ##Torna o texto em minúsculo
+print(texto.capitalize()) ##Pega a string completa e joga apenas o primeiro caractere para maiúsculo
+print(texto.title()) ##Analisa palavra por palavra e torna o primeiro caractere de cada em maiúscula
+print(frase.strip()) ##Remove os espaços em branco no início e final da string --> Nesse caso utilizou-es outra string pra exemplificar
+print(frase.rstrip())##Remove espaços à direita da string
+print(frase.lstrip())##Remove espaços à esquerda
+
+##Divisão
+print(texto.split()) ##Divisão dentro da string considerando os espaços entre as palavras
+
+##Junção
+teste = texto.split()
+print('-'.join(teste)) ##Junta os elementos da string com o elemento colocado 
---
+++ AULAS/AULA09.md
@@ -0,0 +1,40 @@
+# Aula 09 - Manipulação de Textos em Python
+
+## Fatiamento
+Aqui podemos encontrar apenas os comandos utilizados para fatiar os elementos de uma string. Assim podemos dividir o elementos de uma string e buscá-los da maneira que desejarmos.
+
+Esses elementos podem ser acessados, a partir de seu índice, que é o seu menor nível de busca até o maior nível que é a própria string que está sendo trabalhada .
+
+> [!IMPORTANT]
+Vale lembrar que na linguagem de programação Python ocorrre a diferenciação entre letras maiúsculas e minúsculas.
+
+|||
+|-|-----------------------------|
+
+## Análise
+Este tópico vai tratar de assuntos relacionados a análise da string.
+
+Quando precisarmos saber a quantidade de elementos que há em uma string ou fazer uma busca por determinado elementos presente. 
+
+É possível, inclusive, fazer a contagem de elementos juntamente com o fatiamento, algo que foi apresentado no tópico passado.
+
+> [!CAUTION]
+Apesar de podermos fazer o fatiamento juntamente com a contagem utilizando a funcionalidade presente nesse tópico, as funções, aqui apresentada, **NÃO REALIZAM** fatiamento, são exclusivas para a análise da string. 
+
+|||
+|-|-|
+## Transformação
+Aqui veremos funcionalidades que dizem respeito à formatação da string desejada.
+
+> [!CAUTION]
+Os itens propostos aqui falam apenas de alguns tópicos relacionados à formatação do texto. **NÃO TRATAM DA ESTILIZAÇÃO.**
+
+|||
+|-|-|
+
+## Divisão
+Tópicos relacionados a divisão da string.
+|||
+|-|-|
+## Junção
+Trata sobre algumas funcionalidades relacionada a junção 
