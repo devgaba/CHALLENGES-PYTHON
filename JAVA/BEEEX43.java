63c85914b08932f0ef4cd6d153841909c6641d02
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-17 00:39:57 -0300

    JAVA | EXERCICIO BEECROWD 1043

--- JAVA/BEEEX43.java
+++ JAVA/BEEEX43.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class BEEEX43 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
 
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-        double C = sc.nextDouble();
+        double a = entrada.nextDouble();
+        double b = entrada.nextDouble();
+        double c = entrada.nextDouble();
 
-        double calcPerimetro = A + B + C;
+        double perimetro = a + b + c;
+        double area = 0.5 * (c * (a + b));
 
-        System.out.printf("Perimetro = %.1f%n", calcPerimetro);
+        if (a < b + c && b < a + c && c < a + b) {
+            System.out.printf("Perimetro = %.1f\n", perimetro);
+        } else {
+            System.out.printf("Area = %.1f\n", area);
+        }
 
-        sc.close();
+        entrada.close();
     }
 }
-// Aqui fica um comentário a respeito do exercício.
