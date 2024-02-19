a7dc1bb757aa472f401853d49be811a8693a1ff4
recepcao <moneycrashing76@gmail.com>
2024-02-19 10:20:12 -0300

    JAVA | EXERCICIO BEECROWD 1044

--- JAVA/BEEEX44.java
+++ JAVA/BEEEX44.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class BEEEX44 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
-        int a = entrada.nextInt();
-        int b = entrada.nextInt();
+        double a = entrada.nextDouble();
+        double b = entrada.nextDouble();
 
-        int calc = b % a;
-        if (calc > 0.00 && calc < 1.00) {
+        double calc = b % a;
+        double calcDois = a % b;
+        //System.out.printf("%.3f%n", calc);
+        //System.out.printf("%.3f%n", calcDois);
+
+        if (calc == 0.00 || calcDois == 0.00) {
             System.out.println("Sao Multiplos");
         } else {
             System.out.println("Nao sao Multiplos");
