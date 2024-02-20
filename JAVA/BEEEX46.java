39309e72d1b932f6190897ffa130499a83215bba
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-19 23:24:19 -0300

    JAVA | EXERCICIO BEECROWD 1046 - NÃO SOLUCIONADO

---
+++ JAVA/BEEEX46.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BEEEX46 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int horaEntrada = entrada.nextInt();
+        int horaSaida = entrada.nextInt();
+        int dia = 24;
+        int meioDia = dia / 2;
+
+        if (horaSaida < 10) {
+            int calcHora = (dia - horaEntrada) + horaSaida;
+            System.out.println("O JOGO DUROU " + calcHora + " HORA(S)");
+        } else if (horaEntrada <= 10) {
+            int calcHora = (meioDia - horaEntrada) + (horaSaida - meioDia);
+            System.out.println("O JOGO DUROU " + calcHora + " HORA(S)");
+        } else if (horaEntrada > 10 && horaSaida > horaEntrada) {
+            int calcHora = horaSaida - horaEntrada;
+            System.out.println("O JOGO DUROU " + calcHora + " HORA(S)");
+        } else if (horaSaida > 10 && horaEntrada > horaSaida) {
+            int calcHora = (dia - horaEntrada) + horaSaida;
+            System.out.println("O JOGO DUROU " + calcHora + " HORA(S)");
+
+        entrada.close();
+    }
+}
