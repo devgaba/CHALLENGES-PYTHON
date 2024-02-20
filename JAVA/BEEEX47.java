34f0463f75f5b9e3c81659a22cf46c8807a88555
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-20 01:27:59 -0300

    JAVA | EXERCICIO BEECROWD 1047

---
+++ JAVA/BEEEX47.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BEEEX47 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        
+        int horaInicial = entrada.nextInt();
+        int minutoInicial = entrada.nextInt();
+        int horaFinal = entrada.nextInt();
+        int minutoFinal = entrada.nextInt();
+
+        if (horaInicial > horaFinal){
+            int calcDuracao = horaFinal - horaInicial + 24;
+            int calcMinuto = minutoFinal - minutoInicial + 60;
+            System.out.println("O JOGO DUROU " +calcDuracao+ " HORA(S) E " + calcMinuto + " MINUTO(S)");
+        }
+        entrada.close();
+    }
+}
