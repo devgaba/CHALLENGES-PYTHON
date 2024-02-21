64f7623f3b99b8188c52bcde7fb3a446419e6b3b
recepcao <moneycrashing76@gmail.com>
2024-02-21 17:26:00 -0300

    JAVA | EXERCICIO BEECROWD 1059

---
+++ JAVA/BEEEX59.java
@@ -0,0 +1,11 @@
+public class BEEEX59 {
+    public static void main(String[] args) {
+
+        int variavel = 0;
+        int maximo = 50;
+        for (int i = 0; i < maximo; i++) {
+            variavel += 2;
+            System.out.println(variavel);
+        }
+    }
+}
