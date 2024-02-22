8973570a60420d20387e0e3ba203d64c5d504bb6
recepcao <moneycrashing76@gmail.com>
2024-02-22 09:15:47 -0300

    JAVA | EXERCICIO BEECROWD 1060

--- JAVA/BEEEX60.java
+++ JAVA/BEEEX60.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class BEEEX60 {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
         int valorPositivo = 0;
-        int valorDigitado = entrada.nextInt();
-        int[] valores = {};
 
-        for (int i=0; valorDigitado , 0) {
-            valorPositivo++;
+        double[] valores = new double[6];
+
+        for (int i = 0; i < valores.length; i++) {
+            double valorDigitado = entrada.nextDouble();
+            if (valorDigitado > 0) {
+                valorPositivo += 1;
+            }
         }
+        System.out.println(valorPositivo + " valores positivos");
+
         entrada.close();
     }
 }
