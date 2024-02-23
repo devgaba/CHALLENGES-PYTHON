355dd4c5e3220285289bd8a31f83ddd0180df8b2
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-22 23:23:55 -0300

    JAVA | EXERCICIO BEECROWD 1070 - NÃO SOLUCIONADO

--- JAVA/BEEEX70.java
+++ JAVA/BEEEX70.java
@@ -4,6 +4,17 @@ public class BEEEX70 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
+        int valorDigitado = entrada.nextInt();
+
+        for (int i = 0; i < valorDigitado; i++) {
+            if (valorDigitado % 2 == 0) {
+                valorDigitado += 1;
+                System.out.println(valorDigitado);
+            } else {
+                valorDigitado += 2;
+                System.out.println(valorDigitado);
+            }
+        }
         entrada.close();
     }
 }
