d735a3deb0355256f1f92eadda010fdec5fe1ce4
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-22 23:16:19 -0300

    JAVA | EXERCICIO BEECROWD 1070 - ESTRUTURA APENAS

---
+++ JAVA/BEEEX70.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BEEEX70 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        entrada.close();
+    }
+}
