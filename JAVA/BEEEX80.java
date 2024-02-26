bcb5c71d09c81613ed5a50bb47dcf5e2e7483750
recepcao <moneycrashing76@gmail.com>
2024-02-26 15:40:01 -0300

    JAVA | EXERCICIO BEECROWD 1080

--- JAVA/BEEEX80.java
+++ JAVA/BEEEX80.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 public class BEEEX80 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        int[] valores = new int[100];
+        for (int i = 0; i < valores.length; i++) {
+            int valorDigitado = entrada.nextInt();
+            valores[i] = valorDigitado;
+        }
+        int maior = valores[0];
+        int posicao = 0;
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] > maior) {
+                maior = valores[i];
+                posicao = i;
+            }
+        }
+        maior += 0;
+        posicao += 1;
+        System.out.println(maior);
+        System.out.println(posicao);
+
         entrada.close();
     }
 }
