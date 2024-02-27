4013236c4134fb5e62e7394ccb0770670f7f6cf3
Gabriel Henrique <souza.rocha2000@gmail.com>
2024-02-26 21:18:12 -0300

    JAVA | EXERCICIO BEECROWD 1094 - NÃO SOLUCIONADO

--- JAVA/BEEEX94.java
+++ JAVA/BEEEX94.java
@@ -6,15 +6,24 @@ public class BEEEX94 {
 
         int quantCasos = entrada.nextInt();
         int[] casos = new int[quantCasos];
-        String[] letras = { "C", "R", "S" };
 
         for (int i = 0; i < casos.length; i++) {
             int quant = entrada.nextInt();
-            char letra = entrada.next().charAt(0);
-
-            entrada.nextLine();
+            String letra = entrada.nextLine();
+            if (letra == "C") {
+                quantC = quant + 0;
+            } else if (letra == "R") {
+                quantR = quant + 0;
+            } else if (letra == "S") {
+                quantS = quant + 0;
+            }
+            int quantR = 0;
+            int quantS = 0;
+            int quantC = 0;
+            System.out.println("Total de coelhos; " + quantC);
+            System.out.println("Total de coelhos; " + quantR);
+            System.out.println("Total de coelhos; " + quantS);
         }
-
         entrada.close();
     }
 }
